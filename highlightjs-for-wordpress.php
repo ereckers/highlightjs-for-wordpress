89f6a32972f7982ce0b8c6d22ba8842cbae4706f
Ed Reckers <ed@redbridgenet.com>
2014-05-15 16:29:28 -0700

    cleanup and formatting

--- highlightjs-for-wordpress.php
+++ highlightjs-for-wordpress.php
@@ -3,15 +3,15 @@
 Plugin Name: highlight.js for WordPress
 Plugin URI: https://github.com/ereckers/highlightjs-for-wordpress
 Description: highlight.js as a WordPress plugin.
-Version: 1.0.0
+Version: 1.0.1
 Author: Ed Reckers (Red Bridge Internet)
 Author URI: http://www.redbridgenet.com
 License: GPL2
 
 ----------------------------------------------------------------------------
 
-Thanks to the authors of highlight.js:
-https://github.com/isagalaev/highlight.js/blob/master/AUTHORS.en.txt
+Thanks to highlightjs.org and its authors:
+: https://github.com/isagalaev/highlight.js/blob/master/AUTHORS.en.txt
 
 ----------------------------------------------------------------------------
 
@@ -38,47 +38,32 @@ class HighlightJSForWordPress {
 	 * Initialization, Hooks, and localization
 	 */
 	function init() {
-
 		add_action( 'wp_enqueue_scripts', array( $this, 'highlightjs_fwp_enqueue_scripts' ) );
 		add_action( 'wp_head', array( $this, 'highlightjs_fwp_insert_script' ) );
-
 		self::settings();
-
 	}
 
 	/**
 	 * Setup the plugin WP settings and options
 	 */
 	function settings() {
-        // Create array of default settings
-        $this->defaultsettings = array(
-            'color_scheme'    => 'default.css',
-            'custom_selector' => 'pre'
-        );
+		// Create array of default settings
+		$this->defaultsettings = array(
+			'color_scheme'    => 'default.css',
+			'custom_selector' => 'pre'
+		);
 
-        // Create the settings array by merging the user's settings and the defaults
-        $usersettings = (array) get_option('highlightjs_fwp_settings');
-        $this->settings = wp_parse_args( $usersettings, $this->defaultsettings );
+		// Create the settings array by merging the user's settings and the defaults
+		$usersettings = (array) get_option('highlightjs_fwp_settings');
+		$this->settings = wp_parse_args( $usersettings, $this->defaultsettings );
 	}
 
 	/**
 	 * Enqueue highlight.js Script and Style site-wide.
 	 */
 	function highlightjs_fwp_enqueue_scripts() {
-
-		wp_enqueue_style(
-			'highlightjs',
-			plugins_url( '/highlight/styles/'.$this->settings['color_scheme'], __FILE__ ),
-			array(),
-			'1.0.0'
-		);
-		wp_enqueue_script(
-			'highlightjs',
-			plugins_url( '/highlight/highlight.pack.js' , __FILE__ ),
-			array(),
-			'1.0.0',
-			'true'
-		);
+		wp_enqueue_style( 'highlightjs', plugins_url( '/highlight/styles/'.$this->settings['color_scheme'], __FILE__ ), array(), '1.0.1' );
+		wp_register_script( 'highlightjs', plugins_url( '/highlight/highlight.pack.js' , __FILE__ ), array(), '1.0.1', 'true' );
 	}
 
 	/**
