b410a0d7d650a7436128f1fc984c3eddc6d79ae1
Ed Reckers <ed@redbridgenet.com>
2014-05-15 16:45:14 -0700

    code cleanup for the color options

--- utils.php
+++ utils.php
@@ -1,24 +1,14 @@
 <?php
 /**
- * Print Combobox With Styles for Settings
+ * Print Color Scheme stylesheet options
  */
 function highlightjs_fwp_get_style_list( $style = "default.css" ) {
-
 	$options = "";
-	//$style = "default.css";
 
-    //$styleDir = '..' . '/' . PLUGINDIR . '/' . dirname(plugin_basename(__FILE__)) . '/highlight/' . 'styles'; # dirty hack
-
-    //if ( $dir = opendir( $styleDir ) ) {
     if ( $dir = opendir( plugin_dir_path( __FILE__ ) . '/highlight/styles' ) ) {
         while ( $file = readdir( $dir ) ) {
             if ( ( $file == '.' ) or ( $file == '..' ) ) continue;
-
-            if ( $file != $style ) {
-                $options .= "<option value=" . $file . ">$file</option>";
-			} else {
-                $options .= "<option selected=\"selected\">$file</option>";
-			}
+			$options .= "<option value=\"".$file."\" ".selected( $file, $style, false ).">".$file."</option>";
         }
     }
     closedir( $dir );
